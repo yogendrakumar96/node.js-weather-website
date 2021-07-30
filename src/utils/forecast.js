6a10d94718daca7c20c6e2bc5255daac2ff4be60
Yogendra Kumar Vatti <yogendrakumarvatti1996@gmail.com>
2021-07-30 15:21:37 +0530

    add forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location!', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + ' degrees.The Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
